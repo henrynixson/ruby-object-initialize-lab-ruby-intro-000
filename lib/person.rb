d5d0eaeaf78eb76befbc07bf649c70dd879c3f72
Nixson Henry <henrynixon.hn@gmail.com>
2019-02-20 12:00:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,3 @@
-# class Person
-#   def initialize(name)
-#     @name = name
-#   end
-  
-#   def name
-#     @name = name
-#   end
-  
-#   def name
-#     @name
-#   end
-# end
 
 class Person
   def initialize(name)
