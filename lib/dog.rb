83e14234c3be4aba8adf60f005a54ba86a4ff839
Nixson Henry <henrynixon.hn@gmail.com>
2019-02-20 12:00:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,4 @@
-# class Dog 
 
-#   def initialize(name, breed="Mut")
-#     @name = name
-#     @breed = breed
-#   end
-
-#   def name=(name)
-#     @name = name
-#   end
-
-#   def name 
-#     @name
-#   end
-
-# end
- 
 class Dog
   def initialize(name, breed = "Mutt")
   @name = name
